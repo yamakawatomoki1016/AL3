ca2116ac0bcc154b17a5cd64b0520f51ff821f4c
知己 山川 <tomoki10160613@gmail.com>
2023-04-21 16:34:23 +0900

    a

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -8,7 +8,6 @@
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
-#include "DebugCamera.h"
 
 /// <summary>
 /// ゲームシーン
@@ -47,13 +46,6 @@ private: // メンバ変数
 	Audio* audio_ = nullptr;
 	uint32_t textureHandle_ = 0;
 	Sprite* sprite_ = nullptr;
-	Model* model_ = nullptr;
-	WorldTransform worldTransform_;
-	ViewProjection viewProjection_;
-	uint32_t soundDataHandle_ = 0;
-	uint32_t voiceHandle_ = 0;
-	float inputFloat3[3] = {0, 0, 0};
-	DebugCamera* debugCamera_ = nullptr;
 
 	/// <summary>
 	/// ゲームシーン用
