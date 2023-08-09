4d960371efcce0a987e6f3f0c63d61414cecbb23
知己 山川 <tomoki10160613@gmail.com>
2023-08-09 15:50:48 +0900

    aa

--- EnemyBullet.cpp
+++ EnemyBullet.cpp
@@ -8,6 +8,12 @@ void EnemyBullet::Initialize(Model* model, const Vector3& position, const Vector
 	worldTransform_.Initialize();
 	worldTransform_.translation_ = position;
 	velocity_ = velocity;
+	worldTransform_.scale_.x = 0.5f;
+	worldTransform_.scale_.y = 0.5f;
+	worldTransform_.scale_.z = 3.0f;
+	worldTransform_.rotation_.y = std::atan2(velocity_.x,velocity_.z);
+	float velocityXZ = Length({velocity_.x, 0.0f, velocity_.z});
+	worldTransform_.rotation_.x = std::atan2(-velocity_.y, velocityXZ);
 }
 
 void EnemyBullet::Update() {
