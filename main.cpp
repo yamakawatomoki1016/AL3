187e966944a8be4c69edfc9b11ae6227125cdf98
知己 山川 <tomoki10160613@gmail.com>
2023-08-31 21:59:34 +0900

    name change

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"LE2B_27_ヤマカワ_トモキ_単体の敵");
+	win->CreateGameWindow(L"LE2B_27_ヤマカワ_トモキ_複数の敵");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
